7046644fc7a7759ef213683b6db52f42cce2fd45
barna154 <ajr@DESKTOP-BR182EM.localdomain>
2025-09-21 17:18:39 +0200

    Valtoztatasok a sim_zby_barna projektben

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             ]
         ),
         # Node(
-        #     package='ros2_cpp_template',
+        #     package='sim_zby_barna',
         #     executable='simple_sub_node',
         #     output='screen',
         # ),
